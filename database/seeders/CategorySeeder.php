b51c90d8e67484335b5806271f6e0b50c5ee8b2a
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-02-24 23:57:17 +0500

    solve issues in seeder and migration

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -33,6 +34,6 @@ class CategorySeeder extends Seeder
                 'updated_at'=>now(),
             ],
         ];
-        Category::create($categories);
+        DB::table('categories')->insert($categories);
     }
 }
